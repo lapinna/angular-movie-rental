f930a7c41c8ec3616e49e9e65061027678c7552f
Lapina <lapinna.inese@gmail.com>
2023-06-16 15:41:09 +0300

    add login, register, header, footer components and style

---
+++ src/app/components/login/login.component.ts
@@ -0,0 +1,27 @@
+import { Component } from '@angular/core';
+//import { FormControl, FormGroup, ReactiveFormsModule } from "@angular/forms";
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  // template: `
+  //       <section class="login-container">
+  //       <form>
+  //         <label for="email">Email</label>
+  //         <input id="email" type="email" formControlName="email">
+
+  //         <label for="password">Password</label>
+  //         <input id="password" type="text" formControlName="password">
+
+  //         <button type="submit" class="primary">Sign in</button>
+  //       </form>
+  //     </section>
+  // `,
+  styleUrls: ['./login.component.scss']
+})
+export class LoginComponent {
+  // applyForm = new FormGroup({
+  //   email: new FormControl(''),
+  //   password: new FormControl(''),
+  // });
+}
